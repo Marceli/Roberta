d046e61575eacaaab941dbf2a3bbff1235f2f4c5
Marceli <marcel.piekalkiewicz@gmail.com>
2010-03-17 12:29:57 +0000

    posprzatane

--- 2_BLL/IValidable.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-
-namespace Bll
-{
-    public interface IValidable
-	{
-		bool Validate(List<string> ValidationError);
-	}
-}
--- PredicatorTest/PredicatorTest/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PredicatorTest.MvcApplication" Language="C#" %>
